995e2ef715fa3b776c4d9c7859e672eda4cab25d
williamwinter2008-prog <williamwinter2008@gmail.com>
2026-08-04 14:42:57 +0100

    Create discord.js

---
+++ api/auth/api/auth/discord.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+    const clientId = process.env.DISCORD_CLIENT_ID;
+    const clientSecret = process.env.DISCORD_CLIENT_SECRET;
+
+    const redirectUri =
+        "https://chill-zone-dashboard11-iqmzzpb1c-discord-bot9.vercel.app/api/auth/discord";
+
+    const discordUrl =
+        "https://discord.com/oauth2/authorize" +
+        "?client_id=" + clientId +
+        "&response_type=code" +
+        "&redirect_uri=" + encodeURIComponent(redirectUri) +
+        "&scope=identify%20guilds";
+
+    res.redirect(302, discordUrl);
+}
