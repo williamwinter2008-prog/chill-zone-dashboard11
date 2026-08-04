460b39e63bf9ae6f99ca887e29cad78738bcc2c5
williamwinter2008-prog <williamwinter2008@gmail.com>
2026-08-04 15:19:36 +0100

    Update discord.js

--- api/auth/discord.js
+++ api/auth/discord.js
@@ -1,1 +1,15 @@
+export default function handler(req, res) {
+    const clientId = process.env.DISCORD_CLIENT_ID;
 
+    const redirectUri =
+        "https://chill-zone-dashboard11-iqmzzpb1c-discord-bot9.vercel.app/api/auth/discord";
+
+    const discordUrl =
+        "https://discord.com/oauth2/authorize" +
+        "?client_id=" + clientId +
+        "&response_type=code" +
+        "&redirect_uri=" + encodeURIComponent(redirectUri) +
+        "&scope=identify%20guilds";
+
+    res.redirect(302, discordUrl);
+}
